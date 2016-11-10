0e7bd977c7626f42a302bf7d1951ccc295b993e3
Vebjørn Isaksen <vebjorn.isaksen@gmail.com>
2016-11-10 16:08:12 +0100

    Driver stage 2 done, I think :3

--- local_src/driver-gamepad-1.0/driver-gamepad.c
+++ local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -1,17 +1,37 @@
 /*
- * This is a demo Linux kernel module.
+ * This is a demo Linux kernel module. LDD p. 76
  */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
-#include <errno.h>
+#include <linux/kdev_t.h>
+#include <linus/fs.h>
+#include <linux/cdev.h>
+#include <linux/interrupt.h>
+
 #include "efm32gg.h"
 
-#define NAME = "GPIOGamepad"
+
+/* Constants */
+#define DEVICE_NAME = "GPIOGamepad"
+#define DEVICE_COUNT = 1
+
+dev_t device_number;
+struct cdev gamepad_cdev;
+struct class* device_class;
+
+
+/* Avaiable operations on this driver */
+struct file_operations gamepad_fops = {
+	.owner = THIS_MODULE,
+	.open = gamepad_open,
+  .read = gamepad_read,
+	.release = gamepad_release,
+};
 
 /*
- * template_init - function to insert this module into kernel space
+ * Function to insert this module into kernel space
  *
  * This is the first of two exported functions to handle inserting this
  * code into a running kernel
@@ -21,39 +41,66 @@
 
 static int __init gamepad_init(void)
 {
-	printk(KERN_ALERT "Init starting\n");
+	printk(KERN_ALERT "Module initialization begin.\n");
 
-	// Request reservation of GPIO pins used by gamepad, preventing overlapping calls from other drivers
-	if (request_mem_region(GPIO_PA_BASE, 0x24, NAME) == NULL)
+	int value;
+
+	/* Obtain device numbers (LDD p. 45) */
+	value = alloc_chrdev_region(&device_number, 0, DEVICE_COUNT, DEVICE_NAME);
+	if (value < 0)
 	{
-		printk(KERN_ALERT "Failed to reserve GPIO_PA_BASE\n");
-		return EIO; // I/O error, error code 5
+		printk(KERN_ERR "Failed to dynamically-allocate device numbers.");
+		return -1;
 	}
 
-	// TODO????? Enable GPIO clock here?
+	/* Create device node */
+	device_class = class_create(THIS_MODULE, DEVICE_NAME);
+	device_create(device_class, NULL, device_number, NULL, DEVICE_NAME);
 
-	// Setup GPIO as written on page 28 in the Lab Exercise Document
+	/* Char device registration (LDD p. 55) */
+	cdev_init(&gamepad_cdev, &gamepad_fops);
+	gamepad_cdev.owner = THIS_MODULE;
+	cdev_add(&gamepad_cdev, device_number, DEVICE_COUNT);
+
+	/* Request reservation of GPIO pins used by gamepad, preventing overlapping calls from other drivers */
+	if (request_mem_region(GPIO_PC_BASE, 0x24, NAME) == NULL)
+	{
+		printk(KERN_ALERT "Failed to reserve GPIO_PC_BASE in memory.\n");
+		return -1;
+	}
+
+	/* Setup GPIO as written on page 28 in the Lab Exercise Document */
 	iowrite32(0x33333333, GPIO_PC_MODEL);
 	iowrite32(0xff, GPIO_PC_DOUT);
 
-	// TODO: Create interrupt handlers here
+	/* Register interrupt handlers. GPIO_EVEN = 17 and GPIO_ODD = 18 */
+	value = request_irq(17, (irq_handler_t)gamepad_interrupt_handler, 0, DEVICE_NAME, &gamepad_cdev);
+	if (value < 0)
+	{
+		printk(KERN_ERR "Failed to request irq even.");
+		return -1;
+	}
+	value = request_irq(18, (irq_handler_t)gamepad_interrupt_handler, 0, DEVICE_NAME, &gamepad_cdev);
+	if (value < 0)
+	{
+		printk(KERN_ERR "Failed to request irq odd.");
+		return -1;
+	}
 
-	// Setup GPIO interrupts as written on page 28 in the Lab Exercise Document
+	/* Setup GPIO interrupts as written on page 28 in the Lab Exercise Document */
 	iowrite32(0x22222222, GPIO_EXTIPSELL);
 	iowrite32(0xff, GPIO_EXTIFALL);
 	iowrite32(0xff, GPIO_EXTIRISE);
 	iowrite32(0xff, GPIO_IEN);
-	iowrite32(0x802, ISER0); // enables interrupt handling
-
-	// TODO: do more stuff here
+	iowrite32(0x802, ISER0); // Enables interrupt handling
 
 
-	printk(KERN_ALERT "Init done\n");
+	printk(KERN_ALERT "Module initialization done.\n");
 	return 0;
 }
 
 /*
- * template_cleanup - function to cleanup this module from kernel space
+ * Function to cleanup this module from kernel space
  *
  * This is the second of two exported functions to handle cleanup this
  * code from a running kernel
@@ -61,16 +108,69 @@ static int __init gamepad_init(void)
 
 static void __exit gamepad_exit(void)
 {
-	printk(KERN_ALERT "Exit starting\n");
+	printk(KERN_ALERT "Exit starting.\n");
 
-	// Release reservation of GPIO pins used by gamepad
-	release_mem_region(GPIO_PA_BASE, 0x24);
+	// iowrite32(0x0000, GPIO_IEN); for disabling interrupts
 
-	printk(KERN_ALERT "Exit done\n");
+	printk(KERN_DEBUG "Freeing interrupts.\n");
+	free_irq(17, &gamepad_cdev);
+	free_irq(18, &gamepad_cdev);
+
+	printk(KERN_DEBUG "Releasing reservation of GPIO pins used by gamepad.\n");
+	release_mem_region(GPIO_PC_BASE, 0x24);
+
+	printk(KERN_DEBUG "Removing char device from system.\n");
+	device_destroy(device_class, device_number);
+	class_destroy(device_class);
+	cdev_del(&gamepad_cdev);
+
+	printk(KERN_DEBUG "Deallocating device numbers\n");
+	unregister_chrdev_region(device_number, DEVICE_COUNT);
+
+	printk(KERN_ALERT "Exit done.\n");
 }
 
+/* Interrupt handler between hardware and the driver */
+irqreturn_t gamepad_interrupt_handler(int irq, void* dev_id, struct pt_regs* regs)
+{
+	printk(KERN_DEBUG "Handling interrupt.");
+	iowrite32(ioread32(GPIO_IF), GPIO_IFC); // Clear interrupt
+	return IRQ_HANDLED;
+}
+
+/* This is the functions that can be called from user space (from LDD p. 58) */
+
+/* Not useful since all configurations is done in gamepad_init */
+static int gamepad_open(struct inode* inode, struct file* filp)
+{
+	printk(KERN_INFO "Gamepad driver opened.\n");
+	return 0;
+}
+
+/* Not useful since all releasing is done in gamepad_exit */
+int gamepad_release(struct inode *inode, struct file *filp)
+{
+  printk(KERN_INFO "Gamepad driver released.\n");
+	return 0;
+}
+
+/* Read buttons status and copy from kernel space to user space */
+static ssize_t gamepad_read(struct file* filp, char* __user buff, size_t count, loff_t* offp)
+{
+    uint32_t data = ioread32(GPIO_PC_DIN);
+    int value = copy_to_user(buff, &data, 1);
+		if (value > 0)
+		{
+			printk(KERN_ALERT "Could not copy all bytes from GPIO_PC_DIN.");
+			return 0;
+		}
+    return 1;
+}
+
+/* Module configurations */
 module_init(gamepad_init);
 module_exit(gamepad_exit);
 
 MODULE_DESCRIPTION("Small module, demo only, not very useful.");
+MODULE_VERSION("v1.0.0");
 MODULE_LICENSE("GPL");
